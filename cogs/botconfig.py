d7e87a02f4f46c7ac46bf78b432675e9317b7c80
nathanvdh <a1743599@student.adelaide.edu.au>
2020-11-28 21:07:12 +1030

    Remove redundant code and make a cog error handler

--- cogs/botconfig.py
+++ cogs/botconfig.py
@@ -3,7 +3,7 @@
 from discord.ext import commands
 from discord.utils import get
 import checks
-class BotConfig(commands.Cog, command_attrs=dict(hidden=True)):
+class BotConfig(commands.Cog, name='botconfig'):#, command_attrs=dict(hidden=True)):
 	"""Cog for configuring the bot from within discord"""
 	def __init__(self, bot):
 		self.bot = bot
@@ -13,33 +13,38 @@ class BotConfig(commands.Cog, command_attrs=dict(hidden=True)):
 		"""Check if user has admin role"""
 		return checks.is_admin(ctx)
 
-	@commands.command(name='load')
+	async def cog_command_error(self, ctx, error):
+		if hasattr(ctx.command, 'on_error'):
+			return
+		if isinstance(error, commands.MissingRequiredArgument):
+			await ctx.send_help(ctx.command)
+		else:
+			await ctx.send(f'**`ERROR:`** {type(error).__name__} - {error}')
+
+	@commands.group()
+	async def cog(self, ctx):
+		"""Manage cogs"""
+		if ctx.invoked_subcommand is None:
+			await ctx.send_help(ctx.command)
+
+	@cog.command(name='load')
 	async def load_cog(self, ctx, *, cog: str):
-		"""Loads an extension"""
-		try:
-			self.bot.load_extension("cogs."+cog)
-		except Exception as e:
-			await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
-		else:
-			await ctx.send('**`SUCCESS`**')
-	@commands.command(name='unload')
+		"""Loads a cog"""
+		self.bot.load_extension("cogs."+cog)
+		await ctx.send('**`Successfully loaded {}`**'.format(cog))
+	
+	@cog.command(name='unload')
 	async def unload_cog(self, ctx, *, cog: str):
-		"""Unloads an extension"""
-		try:
-			self.bot.unload_extension("cogs."+cog)
-		except Exception as e:
-			await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
-		else:
-			await ctx.send('**`SUCCESS`**')
-	@commands.command(name='reload')
+		"""Unloads a cog"""
+		self.bot.unload_extension("cogs."+cog)
+		await ctx.send('**`Successfully unloaded {}`**'.format(cog))	
+	
+	@cog.command(name='reload')
 	async def reload_cog(self, ctx, *, cog: str):
-		"""Reloads an extension"""
-		try:
-			self.bot.unload_extension("cogs."+cog)
-			self.bot.load_extension("cogs."+cog)
-		except Exception as e:
-			await ctx.send(f'**`ERROR:`** {type(e).__name__} - {e}')
-		else:
-			await ctx.send('**`SUCCESS`**')
+		"""Reloads a cog"""
+		self.bot.unload_extension("cogs."+cog)
+		self.bot.load_extension("cogs."+cog)
+		await ctx.send('**`Successfully reloaded {}`**'.format(cog))
+
 def setup(bot):
 	bot.add_cog(BotConfig(bot))
