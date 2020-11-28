1a1395b0a91fba71211693a2490334a027eb5276
nathanvdh <a1743599@student.adelaide.edu.au>
2020-11-28 21:06:41 +1030

    Lowercase cog names

--- cogs/dad.py
+++ cogs/dad.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
 
-class Dad(commands.Cog):
-	"""A notes cog, like many telegram bots have"""
+class Dad(commands.Cog, name='dad'):
+	"""Hi ___, I'm dad"""
 	def __init__(self, bot):
 		self.bot = bot
 		self.im = ("I\'m", "i\'m", "Im", "im")
@@ -10,7 +10,6 @@ class Dad(commands.Cog):
 	@commands.Cog.listener()
 	async def on_message(self, message):
 		msg = message.content
-		
 		for prefix in self.im:
 			if msg.startswith(prefix+" "):
 				msg = msg[len(prefix)+1:]
