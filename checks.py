a55c6cbed76a691d354448fb39dba10e1c5cc9af
nathanvdh <a1743599@student.adelaide.edu.au>
2020-11-30 21:48:37 +1030

    Update checks.py

--- checks.py
+++ checks.py
@@ -10,7 +10,8 @@ async def bot_channel_only(ctx):
 			return True
 	bot_channel_ids = await db.fetchall("SELECT channel_id FROM bot_channels WHERE guild_id = ? ;",(ctx.guild.id,))
 	bot_channel_ids_list = [x[0] for x in bot_channel_ids]
-	if not bot_channel_ids_list
+	print(bot_channel_ids_list)
+	if not bot_channel_ids_list:
 		return True
 	if ctx.channel.id in bot_channel_ids_list:
 		return True
