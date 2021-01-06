1575fd58bfa49e75a86a1184a61698567ffd81ef
nathanvdh <a1743599@student.adelaide.edu.au>
2021-01-06 21:01:53 +1030

    disable in DMs

--- cogs/dad.py
+++ cogs/dad.py
@@ -9,7 +9,7 @@ class Dad(commands.Cog, name='dad'):
 	
 	@commands.Cog.listener()
 	async def on_message(self, message):
-		if message.author == self.bot.user:
+		if message.author == self.bot.user or message.guild == None:
 			return
 		msg = message.content
 		for prefix in self.im:
